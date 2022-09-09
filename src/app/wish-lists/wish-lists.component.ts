12d8ca5d3ad45c585af4bcdda0ed6ac0e5ebb236
Dmitry Makhlin <dmitrymakhlin@Dmitrys-MBP.homenet.telecomitalia.it>
2022-09-09 18:45:43 +0200

    initial commit

---
+++ src/app/wish-lists/wish-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-lists',
+  templateUrl: './wish-lists.component.html',
+  styleUrls: ['./wish-lists.component.css']
+})
+export class WishListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
