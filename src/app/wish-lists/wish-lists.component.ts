dbd48b485513b82e0daa454b40780283cfb3e912
Dmitry Makhlin <dmitrymakhlin@Dmitrys-MBP.homenet.telecomitalia.it>
2022-09-10 15:40:51 +0200

    first list object

--- src/app/wish-lists/wish-lists.component.ts
+++ src/app/wish-lists/wish-lists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-wish-lists',
   templateUrl: './wish-lists.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishListsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout () {
+    this.http.post('http://127.0.0.1:8000/logout/', {}).subscribe(res => {
+      alert('logged out');
+      this.router.navigate(['login']);
+    },err=>{
+      alert(err);
+    }
+    )
   }
 
 }
